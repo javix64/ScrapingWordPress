701be068e68533e19aac4a5412cad6b2fde32616
javix64 <jav1.x64@gmail.com>
2022-02-09 19:38:17 +0100

    [ENH] Add functions for download pages and posts

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 
 //////////////////////////////////////////////////////////////////////////
 //
-// If you are reading this, maybe you don't understand it. Me neither :')
+// How i could join pages and posts, because i have copied almost everything
+// split it only in two functions...
+// right now is done
 //
 //////////////////////////////////////////////////////////////////////////
 
@@ -25,22 +27,29 @@ const getAllPosts = (url) =>{
             }
         });
 }
-const getAllPages = (url) => {
-    axios.get(sitePages).then(res =>{
-
-    });
+const getAllPages = (url) =>{
+    return axios
+        .get(url)
+        .then(res =>{
+            const totalPages = parseInt(res.headers['x-wp-totalpages'], 10);
+            const totalItems = parseInt(res.headers['x-wp-total'], 10);
+            return {
+                totalPages,
+                totalItems
+            }
+        });
 }
 
 const listPosts =  async (url) => {
     const urlPosts = await getAllPosts(url).then(e=>{
         const { totalItems, totalPages} = e;
-        console.info('Total posts to download:', totalItems);
-        const urlPages = [];
+        console.info('Total posts for download:', totalItems);
+        const urlPosts = [];
         for (let i = 1; i < totalPages +1; i++) {
             const pagePost = `${sitePosts}&page=${i}`;
-            urlPages.push(pagePost);
+            urlPosts.push(pagePost);
         }
-        return urlPages;
+        return urlPosts;
     })
     const promises = [];
     for (let i = 0; i < urlPosts.length; i++) {
@@ -50,76 +59,76 @@ const listPosts =  async (url) => {
     const mapPosts = (await data).map((element)=>{return element.data})
     return mapPosts;
 }
-//////////////////////////////////////////////////////////////////////////
-//
-// It should be a better way to do the next function, but this is the only that i know is working.
-// If you know some different way, open an issue. Thanks :D.
-//
-//////////////////////////////////////////////////////////////////////////
-const downloadPosts = async (url) =>{
-    await listPosts(url).then(e=>{
-        e.map(i=>{
-            i.map(il=>{
-                console.log(il.title)
-            })
-    })});
+
+const listPages =  async (url) => {
+    const urlPages = await getAllPages(url).then(e=>{
+        const { totalItems, totalPages} = e;
+        console.info('Total pages for download:', totalItems);
+        const urlPages = [];
+        for (let i = 1; i < totalPages +1; i++) {
+            const pagePost = `${sitePages}&page=${i}`;
+            urlPages.push(pagePost);
+        }
+        return urlPages;
+    })
+    const promises = [];
+    for (let i = 0; i < urlPages.length; i++) {
+        promises.push(axios.get(urlPages[i]));
+    }
+    const data = Promise.all(promises).then(e=>{return e});
+    const mapPages = (await data).map((element)=>{return element.data})
+    return mapPages;
 }
 
+const downloadPosts = async (url) =>{
+    const arrayItems = []
+    await listPosts(url).then(list=>{
+        console.log('Downloading posts');
+        list.map(array=>{
+            array.map(el=>{
+                const { date, title, content, link} = el;
+                const item = {
+                    date,
+                    title: title.rendered,
+                    content: content.rendered,
+                    link
+                };
+                arrayItems.push(item);
+            })
+
+        })
+    })
+    const file = `./json_downloaded/${siteUrl}-posts.json`;
+    fs.writeFileSync(file, JSON.stringify(arrayItems));
+    console.info(`Posts downloaded and save it in: ${file}`)
+
+}
+const downloadPages = async (url) =>{
+    const arrayItems = []
+    await listPages(url).then(list=>{
+        console.log('Downloading pages');
+        list.map(array=>{
+            array.map(el=>{
+                const { date, title, content, link} = el;
+                const item = {
+                    date,
+                    title: title.rendered,
+                    content: content.rendered,
+                    link
+                };
+                arrayItems.push(item);
+            })
+
+        })
+    })
+    const file = `./json_downloaded/${siteUrl}-pages.json`;
+    fs.writeFileSync(file, JSON.stringify(arrayItems));
+    console.info(`Pages downloaded and save it in: ${file}`)
+
+}
 
 const main = () => {
     downloadPosts(sitePosts);
+    downloadPages(sitePages);
 }
 main();
-// STRUCTURE
-// get total number of pages from pages/posts from: response.headers['x-wp-totalpages'];
-// get total pages/posts from: response.headers['x-wp-total'];
-// from each page/post extract:
-//      title, link, content
-
-// axios.get(site+postOrPage[0]+websPagesOrPosts).then(r=>{
-//     paginationPosts = parseInt(r.headers['x-wp-totalpages']);
-//     console.log('Total posts: ',parseInt(r.headers['x-wp-total']))
-//     for (let i = 1; i < paginationPosts+1; i++) {
-//         axios.get(site+postOrPage[0]+websPagesOrPosts+'&page='+i).then(r=>{
-//             r.data.forEach(e => {
-//                 let data = {
-//                     title : e.title.rendered,
-//                     link : e.link,
-//                     content : e.content.rendered
-//                 }
-//                 arrPosts.push(data);
-//             });
-//         }).then( r=> {
-//             let jsonPosts = {};
-//             jsonPosts.posts = arrPosts;
-//             fs.writeFileSync('posts.json',JSON.stringify(jsonPosts));
-//             if(paginationPosts===i){
-//                 console.log('All posts written!');
-//             }
-//         })
-//     }
-// })
-
-// axios.get(site+postOrPage[1]+websPagesOrPosts).then(r=>{
-//     paginationPages = parseInt(r.headers['x-wp-totalpages']);
-//     console.log('Total pages: ',parseInt(r.headers['x-wp-total']))
-//     for (let i = 1; i < paginationPages+1; i++) {
-//         axios.get(site+postOrPage[1]+websPagesOrPosts+'&page='+i).then(r=>{
-//             r.data.forEach(e => {
-//                 let data = {
-//                     title : e.title.rendered,
-//                     link : e.link,
-//                     content : e.content.rendered
-//                 }
-//                 arrPages.push(data);
-//             });
-//         }).then( r=> {
-//             let jsonPages = {};
-//             jsonPages.pages = arrPages;
-//             fs.writeFileSync('pages.json',JSON.stringify(jsonPages));
-//             if(paginationPages===i){
-//                 console.log('All pages written!');
-//             }
-//         })
-//     }
-// })
